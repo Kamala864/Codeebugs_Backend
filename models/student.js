a9445f4818f0ac3627d74310cb5ea2fb9ddb34a1
Nitesh Dangal <niteshrd1@gmail.com>
2021-12-14 21:24:37 +0545

    Created registration field

---
+++ models/student.js
@@ -0,0 +1,12 @@
+const mongoose =  require("mongoose");
+
+const schema_student = new mongoose.Schema({
+    full_name : {type: String},
+    email : {type : String},
+    age : {type : String},
+    password : {type :String},
+})
+
+const student = mongoose.model('Student', schema_student)
+
+module.exports = student
