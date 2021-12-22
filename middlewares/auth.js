46fe65347d9eb9aad9fb2088c2e7e9d62e884106
Kamala864 <56905936+Kamala864@users.noreply.github.com>
2021-12-22 19:00:17 +0545

    admin authentication completed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,9 @@
 const jwt=require('jsonwebtoken');
 const User=require('../models/student');
+const Admin=require('../models/admin')
 
 
-//guard
+//for student varification
 module.exports.verifyUser=function(req,res,next){
     try{
         const token= req.headers.authorization.split("")[1];
@@ -23,13 +24,27 @@ module.exports.verifyUser=function(req,res,next){
     }
 }
 
-module.exports.verifyAdmin=function(req,res,next){
-    if (!req.userData){
-        return res.status(401).json({message:"unauthorized"})
+
+
+
+// for admin varification 
+
+module.exports.verifyadmin=function(req,res,next){
+    try{
+        const token= req.headers.authorization.split("")[1];
+        const data=jwt.verify(token,'anysecretkey');
+
+        Admin.findOne({_id:data.yourId})
+        .then(function(result){
+            req.userData=result;
+            next()
+        })
+        .catch(function(e){
+            res.status(401).json({error:e})
+        })
+    }
+    catch(e){
+        res.status(401).json({error:e})
 
     }
-    else if(req.userData.user_catagory=='admin'){
-         return res.status(401).json({message:"Unauthorized"})
-    }
-    next();
 }
