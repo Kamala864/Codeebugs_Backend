3ec1acce05f7e1dcac97165b954c6c2f70114d2a
Roshan Tamang <tgroshan42@gmail.com>
2022-01-27 19:45:53 +0545

    changed opt to otp in otp schema

--- models/otp.js
+++ models/otp.js
@@ -7,13 +7,13 @@ const schema_otp = new mongoose.Schema({
      expire_at: {
           type: Date,
           default: Date.now(),
-          expires: 3540
+          expires: 3600
       }
   })
 
   //deletes automatically after 1 hour
-schema_otp.index({ "expire_at": 1 }, { expireAfterSeconds: 3540 }); 
+schema_otp.index({ "expire_at": 1 }, { expireAfterSeconds: 3600 }); 
 
-const otp = mongoose.model("Opt", schema_otp)
+const otp = mongoose.model("Otp", schema_otp)
 
 module.exports = otp;
